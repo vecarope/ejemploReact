b49b3052730ae069bcaa828374975c2e6ddc8f53
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-03 16:10:14 -0300

    feat: add page for register view

--- src/views/Register/index.jsx
+++ src/views/Register/index.jsx
@@ -2,9 +2,9 @@ import Register from '../../components/Register';
 
 const RegisterPage = () => {
   return (
-    <>
+    <div class="bg-gradient-to-b from-mid-light-blue via-mid-blue to-dark-purple min-h-screen flex justify-center items-center h-screen">
       <Register />
-    </>
+    </div>
   );
 };
 
