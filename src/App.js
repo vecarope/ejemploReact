05c339445939c05284982625b0d68636cfecddf7
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-03 00:42:02 -0300

    feat: add register route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 const HomePage = React.lazy(() => import('./views/Home'));
+const RegisterPage = React.lazy(() => import('./views/Register'));
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
         </Route>
+        <Route path="register" element={<RegisterPage />} />
       </Routes>
     </Suspense>
   );
