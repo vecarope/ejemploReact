632d9be28db83a59589f0a69c44cd974b49c2223
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-07 14:43:15 -0300

    feat: update styles howwework

--- src/components/Landing/HowWeWork.jsx
+++ src/components/Landing/HowWeWork.jsx
@@ -3,12 +3,12 @@ import coverVideo from '../../assets/design/cover-video.png';
 const HowWeWork = () => {
   // const urlVideo = 'https://www.youtube.com/watch?v=z5NPi7WrViU';
   return (
-    <section class="flex justify-center items-center h-screen md:h-2/3 flex-col md:flex-row">
-      <div class="md:text-center w-96 md:w-2/4">
+    <section class="flex justify-center items-center h-screen md:h-2/3 flex-col  md:flex-row">
+      <div class="md:text-center w-96 md:w-2/4 ">
         <h2 class="font-sans text-5xl md:text-3xl font-semibold text-dark-purple">
           ¿Cómo funcionamos?
         </h2>
-        <h5 class="py-4 font-sans text-xl font-medium   ">
+        <h5 class="py-4 font-sans text-xl font-medium md:w-4/5 md:mx-auto">
           Da click a nuestro video y en sólo 60 segundos conoce{' '}
           <span class="text-light-purple">Devsafio</span> y cómo estamos{' '}
           <span class="text-light-purple">cambiando el mundo TI</span>,
