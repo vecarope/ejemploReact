3a54a260500953e14ed0047d9e8fabc10a94e6d6
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-07 10:32:11 -0300

    add how we work section

--- src/components/Landing/HowWeWork.jsx
+++ src/components/Landing/HowWeWork.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import coverVideo from '../../assets/design/cover-video.png';
 const HowWeWork = () => {
+  // const urlVideo = 'https://www.youtube.com/watch?v=z5NPi7WrViU';
   return (
-    <div>
-      <h1>Este es el *HOW WE WORK*</h1>
-    </div>
+    <section class="flex justify-center items-center h-screen md:h-2/3 flex-col md:flex-row">
+      <div class="md:text-center w-96 md:w-2/4">
+        <h2 class="font-sans text-5xl md:text-3xl font-semibold text-dark-purple">
+          ¿Cómo funcionamos?
+        </h2>
+        <h5 class="py-4 font-sans text-xl font-medium   ">
+          Da click a nuestro video y en sólo 60 segundos conoce{' '}
+          <span class="text-light-purple">Devsafio</span> y cómo estamos{' '}
+          <span class="text-light-purple">cambiando el mundo TI</span>,
+          acelerando y potenciando las células tecnológicas.
+        </h5>
+      </div>
+      <div class="md:w-1/4">
+        {/* <iframe src="https://www.youtube.com/embed/z5NPi7WrViU" title="Devsafio" frameBorder={0}> 
+            </iframe>*/}
+        <img src={coverVideo} alt="video devsafio" />
+      </div>
+    </section>
   );
 };
 
