5bc49498279fab0ee3054a0db1e4c0271ea04ec0
Irani <iranimromero@gmail.com>
2022-11-08 01:30:22 -0300

    Add text and button style

--- src/components/Landing/LandingHero.jsx
+++ src/components/Landing/LandingHero.jsx
@@ -4,20 +4,20 @@ import coverImage from '../../assets/design/cover_image.png';
 
 const LandingHero = () => {
   return (
-
-    <section class="bg-gradient-to-b from-mid-light-blue via-mid-blue to-dark-purple min-h-screen flex justify-center items-center h-screen p-[5rem]" id="hero">
+    <section id="hero">
+    <div class="bg-gradient-to-b from-mid-light-blue via-mid-blue to-dark-purple min-h-screen flex justify-center items-center h-screen p-[5rem]" >
 
       <div class="" >
 
         <div class="text-white">
-          <h1 class="">
+          <h1 class="text-5xl py-2">
             <strong>Buscas talento TI?</strong>
           </h1>
           
-          <h2 class="">
-            Contrata <a class="">Talento TI rápido</a>, inteligente y eficiente
+          <h2 class="text-3xl">
+            Contrata <span class="text-light-purple font-semibold">Talento TI</span> rápido, inteligente y eficiente
           </h2>
-          <h3 class="">
+          <h3 class="text-1xl py-2">
             Te encontramos profesionales en 5 días y los acompañamos por 3 meses con tutores senior para potenciar y acelerar sus habilidades técnicas
           </h3>
           <br />
@@ -28,7 +28,7 @@ const LandingHero = () => {
         <div class="">
           <button
             type="submit"
-            class="rounded-lg  bg-white text-center text-mid-light-blue">
+            class="rounded-lg  bg-white text-center text-mid-light-blue p-[1rem] uppercase">
             <strong>Quiero contratar</strong>
           </button>
         </div>
@@ -37,7 +37,7 @@ const LandingHero = () => {
       <div>
         <img src={coverImage} alt="video devsafio" />
       </div>
-
+    </div>
     </section>
 
   );
