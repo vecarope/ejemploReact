6bfe1c6bb321bc7c1161ceefee9599e3e6e68558
Irani <iranimromero@gmail.com>
2022-11-07 21:16:47 -0300

    Add text to Hero section

--- src/components/Landing/LandingHero.jsx
+++ src/components/Landing/LandingHero.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
+
 const LandingHero = () => {
   return (
-    <div>
-      <h1>Este es el *LANDING HERO*</h1>
-    </div>
+
+    <section class="landingHero" id="hero">
+
+      <div class="container " >
+
+        <div class="">
+          <h1 class="">
+            <strong>Buscas talento TI?</strong>
+          </h1>
+          <hr class="" />
+          <h2 class="">
+            Contrata <a class="">Talento TI rápido</a>, inteligente y eficiente
+          </h2>
+          <h3 class="">
+            Te encontramos profesionales en 5 días y los acompañamos por 3 meses con tutores senior para potenciar y acelerar sus habilidades técnicas
+          </h3>
+          <br />
+
+        </div>
+
+
+        <div class="">
+          <button
+            type="submit"
+            class="rounded-lg border hover:border text-lg p-2 w-2/3 font-regular bg-white hover:bg-white hover:md:bg-mid-blue md:bg-light-purple hover: text-blue mb:text-blue hover:text-blue md:text-blue font-sans">
+            Quiero contratar
+          </button>
+        </div>
+
+      </div>
+
+    </section>
+
   );
+
 };
 
 export default LandingHero;
