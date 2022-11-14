4984b6ee805d45d5ead0f38b2a70edacaab0c81d
Irani <iranimromero@gmail.com>
2022-11-13 22:50:54 -0300

    Add a folder for CSS components at Assets, and create CSS file for a button

--- src/components/Landing/LandingHero.jsx
+++ src/components/Landing/LandingHero.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../../assets/componentsCSS/button.css';
 
 const LandingHero = () => {
   return (
     <div>
       <h1>Este es el *LANDING HERO*</h1>
+
+      <div>
+        <h1>Esta es una prueba</h1>
+        <h2>Crear componentes</h2>
+        <button className="btn btn-blue btn-blue:hover">Botón</button>
+      </div>
     </div>
   );
 };
