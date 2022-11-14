106cb4f792c3c655dc13db6594354954a624d3da
Irani <iranimromero@gmail.com>
2022-11-13 23:39:34 -0300

    Add white button

--- src/components/Landing/LandingHero.jsx
+++ src/components/Landing/LandingHero.jsx
@@ -5,12 +5,6 @@ const LandingHero = () => {
   return (
     <div>
       <h1>Este es el *LANDING HERO*</h1>
-
-      <div>
-        <h1>Esta es una prueba</h1>
-        <h2>Crear componentes</h2>
-        <button className="btn btn-blue btn-blue:hover">Botón</button>
-      </div>
     </div>
   );
 };
