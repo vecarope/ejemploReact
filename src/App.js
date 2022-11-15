21ac9143bdbbc1392126def48e0d53976be3f25f
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-15 20:36:38 -0300

    feat: add login route to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 const HomePage = React.lazy(() => import('./views/Home'));
 const RegisterPage = React.lazy(() => import('./views/Register'));
+const LoginPage = React.lazy(() => import(`./views/Login`));
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
           <Route path="register" element={<RegisterPage />} />
+          <Route path="login" element={<LoginPage />} />
         </Route>
       </Routes>
     </Suspense>
