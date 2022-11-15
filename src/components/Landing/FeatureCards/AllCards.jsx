0b3c5eb76c421815b99bf4fcfa6eb49ed8ae3e39
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-15 13:45:21 -0300

    feat: add all cards components

--- src/components/Landing/FeatureCards/AllCards.jsx
+++ src/components/Landing/FeatureCards/AllCards.jsx
@@ -3,13 +3,14 @@ import { featuresData } from '../../../common/featuresData';
 
 const AllCards = () => {
   return (
-    <div>
+    <div class="flex m-auto items-center justify-center">
       {featuresData.map((ele) => (
         <Card
           head={ele.title}
           pic={ele.cover}
           text={ele.content}
           key={ele.id}
+          id={ele.id}
         />
       ))}
     </div>
