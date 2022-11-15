82540d4214397b28a5416c341fa84c78904feb41
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-15 13:45:38 -0300

    feat: add card component

--- src/components/Landing/FeatureCards/Card.jsx
+++ src/components/Landing/FeatureCards/Card.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 
-const Card = ({ head, pic, text }) => {
+const Card = ({ head, pic, text, id }) => {
   return (
-    <div>
+    <div className="rounded-lg py-4 px-6 shadow-lg md:shadow-none bg-white md:bg-transparent w-fit md:w-1/5">
       <img src={pic} alt="feature images" />
-      <header>{head}</header>
-      <p>{text}</p>
+      <header class="p-6 font-sans text-2xl pt-4 font-semibold text-dark-purple">
+        {head}
+      </header>
+      <p className="text-base">{text}</p>
     </div>
   );
 };
@@ -13,7 +15,8 @@ const Card = ({ head, pic, text }) => {
 Card.propTypes = {
   head: PropTypes.string.isRequired,
   pic: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
 };
 
 export default Card;
