ec26815b60efbcffc1a6857baad2e03521261ab5
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-15 19:36:51 -0300

    feat: add the first version of card

--- src/components/Landing/FeatureCards/Card.jsx
+++ src/components/Landing/FeatureCards/Card.jsx
@@ -2,8 +2,10 @@ import PropTypes from 'prop-types';
 
 const Card = ({ head, pic, text, id }) => {
   return (
-    <div className="rounded-lg py-4 px-6 shadow-lg md:shadow-none bg-white md:bg-transparent w-fit md:w-1/5">
-      <img src={pic} alt="feature images" />
+    <div className="rounded-lg my-4 text-center py-5 px-6 shadow-lg md:shadow-none bg-white md:bg-transparent w-fit md:w-4/12">
+      <div className="flex justify-center">
+        <img src={pic} alt="feature images" />
+      </div>
       <header class="p-6 font-sans text-2xl pt-4 font-semibold text-dark-purple">
         {head}
       </header>
