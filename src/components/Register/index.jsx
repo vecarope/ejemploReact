e0062c71bcc3e32f67b472052c91ea5f6b0b59eb
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-16 16:35:35 -0300

    refactor: import validations from yup file

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,59 +1,17 @@
 import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import { validateSchema } from '../../validation/validateForm';
 import MoonImage from '../../assets/design/moon_color.png';
 import OvalBlue from '../../assets/design/oval.png';
+
+const initialCredentials = {
+  userName: '',
+  userLastName: '',
+  userEmail: '',
+  userPassword: '',
+  passwordConfirm: ''
+};
+
 const Register = () => {
-  const initialCredentials = {
-    userName: '',
-    userLastName: '',
-    userEmail: '',
-    userPassword: '',
-    passwordConfirm: ''
-    // userId: 1
-  };
-  let validateStrings = /^[A-Za-z#&]+$/;
-  let validateAlphanumeric = /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
-
-  const validateSchema = Yup.object().shape({
-    userName: Yup.string()
-      .required('Debes ingresar un nombre.')
-      .matches(validateStrings, {
-        excludeEmptyString: true,
-        message: 'El nombre debe ser solo texto.'
-      })
-      .min(2, 'El nombre debe tener al menos 2 letras.')
-      .max(25, 'El nombre debe ser más corto.'),
-    userLastName: Yup.string()
-      .required('Debes ingresar un apellido.')
-      .matches(validateStrings, {
-        excludeEmptyString: true,
-        message: 'El apellido debe ser solo texto.'
-      })
-      .min(2, 'El apellido debe tener al menos 2 letras.')
-      .max(30, 'El apellido debe ser más corto.'),
-    userEmail: Yup.string()
-      .required('El e-mail es requerido.')
-      .email('El e-mail ingresado no es válido.')
-      .min(6, 'El e-mail es demasiado corto.')
-      .max(50, 'El e-mail ingresado es muy largo.'),
-    userPassword: Yup.string()
-      .matches(validateAlphanumeric, {
-        excludeEmptyString: true,
-        message:
-          'La contraseña debe tener al menos una mayúscula, una minúscula y un número.'
-      })
-      .required('Debes crear una contraseña.')
-      .min(8, 'La contraseña debe tener más de ocho caracteres.')
-      .max(20, 'La contraseña debe ser más corta.'),
-
-    passwordConfirm: Yup.string().when('userPassword', {
-      is: (value) => (value && value.length > 0 ? true : false),
-      then: Yup.string()
-        .oneOf([Yup.ref('userPassword')], 'Las contraseñas no cohinciden.')
-        .required('Debes repetir la contraseña.')
-    })
-  });
-
   return (
     <div class="flex flex-col h-screen md:h-auto items-center md:bg-white md:w-9/12 md:rounded-xl md:border-8 md:border-zinc-800 max-w-screen-xl ">
       <h1 className="mb-5 py-1.5 md:p-8 text-4xl text-white md:text-5xl md:text-dark-purple font-bold text-center font-sans  ">
