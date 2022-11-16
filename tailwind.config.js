919d309fec0d3c2091d754341fca8067fba10731
Irani <iranimromero@gmail.com>
2022-11-16 03:00:16 -0300

    Install plugin Tailwind CSS Custom Forms, add require plugin on tailwind.config.js add style on checkbox file

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,5 +36,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [require('daisyui'), require('tw-elements/dist/plugin')]
+  plugins: [require('daisyui'), require('tw-elements/dist/plugin'), require("@tailwindcss/forms")]
 };
