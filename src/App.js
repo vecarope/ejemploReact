de4e0f18c0c2537768b3e2b564d2536de82d5323
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-22 15:15:18 -0300

    fix: import and export in error404 component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Error404 }  from './views/Error404';
+const Error404 = React.lazy(() => import('./views/Error404'));
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 const HomePage = React.lazy(() => import('./views/Home'));
 const RegisterPage = React.lazy(() => import('./views/Register'));
 const LoginPage = React.lazy(() => import(`./views/Login`));
 const ApplicationFormPage = React.lazy(() => import('./views/ApplicationForm'));
-
-
 
 function App() {
   return (
@@ -18,8 +16,8 @@ function App() {
           <Route path="register" element={<RegisterPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="application" element={<ApplicationFormPage />} />
-          <Route path="/404" element={<Error404/>}/>
-          <Route path="*" element={<Navigate to ="/404"/>}/>
+          <Route path="/404" element={<Error404 />} />
+          <Route path="*" element={<Navigate to="/404" />} />
         </Route>
       </Routes>
     </Suspense>
