f032947ee11b46b59cba5db922c098a593309171
DarkKapo <rodrigo.qc@hotmail.es>
2022-11-22 20:46:51 -0800

    UPDATE refactoring code index.jsx and validateForm.jsx

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -3,7 +3,7 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { validateSchema } from '../../validation/validateForm';
 import MoonImage from '../../assets/design/moon_color.png';
 import OvalBlue from '../../assets/design/oval.png';
-import axios from "axios"
+import axios from "axios";
 
 const initialCredentials = {
   firstName: '',
@@ -15,7 +15,7 @@ const initialCredentials = {
 
 const Register = () => {
   return (
-    <div class="flex flex-col items-center md:bg-white md:w-3/5 md:my-11 md:rounded-xl md:border-8 md:border-zinc-800 max-w-screen-xl ">
+    <div className="flex flex-col items-center md:bg-white md:w-3/5 md:my-11 md:rounded-xl md:border-8 md:border-zinc-800 max-w-screen-xl ">
       <h1 className=" py-1.5 md:my-4 text-4xl text-white md:text-5xl md:text-dark-purple font-bold text-center font-sans  ">
         Regístrate
       </h1>
@@ -23,10 +23,10 @@ const Register = () => {
         <img
           src={OvalBlue}
           alt="oval"
-          class="object-left-top -mx-10 ... opacity-50"
+          className="object-left-top -mx-10 ... opacity-50"
         />
       </div>
-      <section class="md:min-w-full md:justify-center md:flex">
+      <section className="md:min-w-full md:justify-center md:flex">
         <Formik
           initialValues={initialCredentials}
           validationSchema={validateSchema}
@@ -36,27 +36,24 @@ const Register = () => {
             localStorage.setItem('User temp', values);
             alert(JSON.stringify(values));
 
-            console.log(values);
-
             try {
-              const { data } = await axios.post("http://localhost:3001/api/auth/register", values)
-              alert("Te has registrado con éxito")
+              const {data} = await axios.post('http://localhost:3001/api/auth/register', values)
+              alert(data.message);
               window.location.reload()
-              return data
+              return data.message
             } catch ({ response }) {
-              alert(response.data.error)
+              alert(response.data.message)
             }
           }}
         >
           {({ errors, touched, isSubmitting }) => (
             <Form className="form-control max-w-xs md:justify-center ">
-              <div class="md:contents md:items-center">
-                <div class="flex flex-col md:flex-wrap">
-                  <div class="flex flex-col md:flex-wrap">
+              <div className="md:contents md:items-center">
+                <div className="flex flex-col md:flex-wrap">
+                  <div className="flex flex-col md:flex-wrap">
                     <label
                       htmlFor="firstName"
-                      className="label-text"
-                      class="text-white md:text-dark-text md:text-lg pt-3 font-sans md:pr-4"
+                      className="text-white label-text md:text-dark-text md:text-lg pt-3 font-sans md:pr-4"
                     >
                       Nombre:
                     </label>
@@ -71,14 +68,13 @@ const Register = () => {
                       <ErrorMessage
                         component="div"
                         name="firstName"
-                        class="text-red-500"
+                        className="text-red-500"
                       />
                     )}
                   </div>
                   <label
                     htmlFor="lastName"
-                    className="label-text"
-                    class="text-white md:text-dark-text md:text-lg pt-3 font-sans md:pr-4"
+                    className="text-white label-text md:text-dark-text md:text-lg pt-3 font-sans md:pr-4"
                   >
                     Apellido:
                   </label>
@@ -93,16 +89,15 @@ const Register = () => {
                     <ErrorMessage
                       component="div"
                       name="lastName"
-                      class=" text-red-500 "
+                      className=" text-red-500 "
                     />
                   )}
                 </div>
-                <div class="flex flex-col  md:w-fit md:flex-wrap">
-                  <div class="flex flex-col md:w-fit">
+                <div className="flex flex-col  md:w-fit md:flex-wrap">
+                  <div className="flex flex-col md:w-fit">
                     <label
                       htmlFor="email"
-                      className="label-text"
-                      class="text-white md:text-dark-text md:text-lg pt-3 font-sans md:w-80"
+                      class="text-white label-text md:text-dark-text md:text-lg pt-3 font-sans md:w-80"
                     >
                       Ingresa tu correo:
                     </label>
@@ -117,15 +112,14 @@ const Register = () => {
                       <ErrorMessage
                         component="div"
                         name="email"
-                        class="text-red-500"
+                        className="text-red-500"
                       />
                     )}
                   </div>
-                  <div class="flex flex-col md:w-fit">
+                  <div className="flex flex-col md:w-fit">
                     <label
                       htmlFor="password"
-                      className="label-text"
-                      class="text-white md:text-dark-text md:text-lg pt-3 font-sans md:w-80"
+                      className="text-white label-text md:text-dark-text md:text-lg pt-3 font-sans md:w-80"
                     >
                       Crea tu contraseña:
                     </label>
@@ -140,15 +134,14 @@ const Register = () => {
                       <ErrorMessage
                         component="div"
                         name="password"
-                        class="text-red-500"
+                        className="text-red-500"
                       />
                     )}
                   </div>
                   <div class="flex flex-col md:w-fit">
                     <label
                       htmlFor="passwordConfirm"
-                      className="label-text"
-                      class="text-white md:text-dark-text md:text-lg pt-3 font-sans md:w-80"
+                      className="text-white label-text md:text-dark-text md:text-lg pt-3 font-sans md:w-80"
                     >
                       Reingresa contraseña:
                     </label>
@@ -163,21 +156,21 @@ const Register = () => {
                       <ErrorMessage
                         component="div"
                         name="passwordConfirm"
-                        class="text-red-500"
+                        className="text-red-500"
                       />
                     )}
                   </div>
                 </div>
               </div>
-              <div class="flex flex-col py-6 items-center md:justify-center   ">
+              <div className="flex flex-col py-6 items-center md:justify-center   ">
                 <button
                   type="submit"
-                  class="rounded-lg border hover:border text-lg p-2 w-2/3 font-regular bg-white hover:bg-transparent hover:md:bg-mid-blue md:bg-dark-purple hover: text-dark-purple mb:text-white hover:text-white md:text-white font-sans"
+                  className="rounded-lg border hover:border text-lg p-2 w-2/3 font-regular bg-white hover:bg-transparent hover:md:bg-mid-blue md:bg-dark-purple hover: text-dark-purple mb:text-white hover:text-white md:text-white font-sans"
                 >
                   Crear cuenta
                 </button>
                 <Link to={'/login'}>
-                  <span class=" flex py-3.5 text-sm text-white text-center font-thin  hover:md:text-mid-blue md:text-dark-purple font-sans">
+                  <span className=" flex py-3.5 text-sm text-white text-center font-thin  hover:md:text-mid-blue md:text-dark-purple font-sans">
                     Iniciar sesión
                   </span>
                 </Link>
@@ -192,11 +185,11 @@ const Register = () => {
           )}
         </Formik>
       </section>
-      <div class="container">
+      <div className="container">
         <img
           src={MoonImage}
           alt="design moon"
-          class="object-left-bottom -mx-8 relative"
+          className="object-left-bottom -mx-8 relative"
         />
       </div>
     </div>
