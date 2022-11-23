a7ac79bae3392abb4adf567b2c980d5344ad318d
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-22 22:21:13 -0300

    feat: change class for className in all components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import logo from "../../assets/images/DEV-IMAGOTIPO-WHITE-HORIZONTAL.png"
-import { AiOutlineLinkedin } from "react-icons/ai"
+import React from 'react';
+import logo from '../../assets/images/DEV-IMAGOTIPO-WHITE-HORIZONTAL.png';
+import { AiOutlineLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <footer className="footer items-center p-4 bg-[#1E239A] text-neutral-content text-center">
       <div className="items-center grid-flow-col">
-        <img class="w-40" src={ logo } alt="Logo"/>
-      </div> 
-      <p className='justify-center mx-auto'>Copyright © 2022 - Devsafio</p>
-      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a className='w-40' href="https://www.linkedin.com/company/devsafio/" target="_blank" rel="noopener         noreferrer">
-          <AiOutlineLinkedin size="3rem" href='https://www.linkedin.com/company/devsafio/'/>
-        </a> 
+        <img className="w-40" src={logo} alt="Logo" />
       </div>
-  </footer>
+      <p className="justify-center mx-auto">Copyright © 2022 - Devsafio</p>
+      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a
+          className="w-40"
+          href="https://www.linkedin.com/company/devsafio/"
+          target="_blank"
+          rel="noopener         noreferrer"
+        >
+          <AiOutlineLinkedin
+            size="3rem"
+            href="https://www.linkedin.com/company/devsafio/"
+          />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-  )
-}
-
-export default Footer
-        
-
+export default Footer;
--- src/components/Landing/FeatureCards/Card.jsx
+++ src/components/Landing/FeatureCards/Card.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Card = ({ head, pic, text, id }) => {
+const Card = ({ head, pic, text }) => {
   return (
     <div className="rounded-lg my-4 text-center py-5 px-6 shadow-lg md:shadow-none bg-white md:bg-transparent w-fit md:w-4/12">
       <div className="flex justify-center">
@@ -17,8 +17,7 @@ const Card = ({ head, pic, text, id }) => {
 Card.propTypes = {
   head: PropTypes.string.isRequired,
   pic: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired
+  text: PropTypes.string.isRequired
 };
 
 export default Card;
