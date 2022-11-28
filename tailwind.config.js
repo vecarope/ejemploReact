9cac4ff6c3fcdd49056a6aaf2684a5b710980a04
Irani <iranimromero@gmail.com>
2022-11-28 11:37:11 -0300

    Modify bg image to use a theme or a image file

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         'light-purple': '#2738F5',
         'dark-text': '#232323',
         'fill-light': '#E2F2FE'
+      },
+      backgroundImage: {
+        'error404': "url('../../assets/design/error404.png')",
       }
     }
   },
