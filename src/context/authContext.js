593f3abef99ecb576a0ea5caa665fbf3ff65cb45
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-12-07 17:54:13 -0300

    feat: add login context

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,53 +1,48 @@
-import { createContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { createContext, useState } from 'react';
 import { postLoginAxios } from '../hooks/postAxios';
 import Swal from 'sweetalert2';
 
 export const AuthContext = createContext();
 
+const initialUser = localStorage.getItem('user');
+
 export const AuthProvider = ({ children }) => {
-  const [userData, setUserData] = useState(null);
-  // const [token, setToken] = useState(null);
+  const [userData, setUserData] = useState(
+    initialUser ? JSON.parse(initialUser) : null
+  );
 
-  let timerInterval;
-  const navigate = useNavigate();
-
-  const loginStatus = async (user) => {
-    setUserData(user);
-    userData !== null ? navigate('/application') : navigate('/register');
-    console.log(Object.values(user));
-  };
-
-  const logoutStatus = async () => {
+  const userLogout = () => {
     setUserData(null);
-    userData !== null ? navigate('/application') : navigate('/register');
+    localStorage.clear();
   };
 
   const postLogin = async (values) => {
-    const { user } = await postLoginAxios(values);
-    localStorage.setItem('userInfo', { user });
-    console.log('USER DESDE POSTLOGIN -> ', user);
-    loginStatus(localStorage.getItem('userInfo'));
-    console.log('USER DESDE USER -> ', userData);
-    /*  Swal.fire({
-      title: 'Bienvenido!',
-      html: `Bienvenido, ${profile.user.firstName} ${profile.user.lastName},`,
-      timer: 2000,
-      timerProgressBar: true,
-      didOpen: () => {
-        Swal.showLoading();
-        const b = Swal.getHtmlContainer().querySelector('b');
-        timerInterval = setInterval(() => {
-          b.textContent = Swal.getTimerLeft();
-        }, 100);
-      },
-      willClose: () => {
-        clearInterval(timerInterval);
-      }
-    }); */
+    const { user, token } = await postLoginAxios(values);
+    if (user) {
+      setUserData(user);
+      localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('token', token);
+      let timerInterval;
+      Swal.fire({
+        title: 'Bienvenido!',
+        html: `Bienvenido, ${user.firstName} ${user.lastName},`,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const b = Swal.getHtmlContainer().querySelector('b');
+          timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft();
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        }
+      });
+    }
   };
 
-  const data = { userData, postLogin, logoutStatus };
+  const data = { userData, userLogout, postLogin };
 
   return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
 };
