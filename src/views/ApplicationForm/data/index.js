f9fb3b9a5b2463d495bce563a5d4eb676031d3ea
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-12-14 14:37:33 -0300

    feat: add initual values for Form

---
+++ src/views/ApplicationForm/data/index.js
@@ -0,0 +1,19 @@
+export const initialForm = {
+  //Personal information:
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  city: '',
+  country: ''
+  //Education Profile
+  /*  typeInstitution: '',
+  course1: '',
+  Institutecourse1: '',
+  typeInstitute: '' */
+  //JobProfile
+
+  //ExperienceAndWork
+
+  //JobType
+};
