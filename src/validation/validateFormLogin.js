ebbbe258c966fb3ae9ddc3128e3f3bac9a6cc224
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-12-19 10:29:47 -0300

    feat: create the forgot password view

--- src/validation/validateFormLogin.js
+++ src/validation/validateFormLogin.js
@@ -18,3 +18,30 @@ export const validateSchemaLogin = Yup.object().shape({
     .min(8, 'La contraseña debe tener más de ocho caracteres.')
     .max(20, 'La contraseña debe ser más corta.')
 });
+
+export const validateForgotPassword = Yup.object().shape({
+  email: Yup.string()
+    .required('El e-mail es requerido.')
+    .email('El e-mail ingresado no es válido.')
+    .min(6, 'El e-mail es demasiado corto.')
+    .max(50, 'El e-mail ingresado es muy largo.')
+});
+
+export const validateNewPassword = Yup.object().shape({
+  password: Yup.string()
+    .matches(validateAlphanumeric, {
+      excludeEmptyString: true,
+      message:
+        'La contraseña debe tener al menos una mayúscula, una minúscula y un número.'
+    })
+    .required('Debes ingresar una contraseña.')
+    .min(8, 'La contraseña debe tener más de ocho caracteres.')
+    .max(20, 'La contraseña debe ser más corta.'),
+
+  passwordConfirm: Yup.string().when('password', {
+    is: (value) => (value && value.length > 0 ? true : false),
+    then: Yup.string()
+      .oneOf([Yup.ref('password')], 'Las contraseñas no cohinciden.')
+      .required('Debes repetir la contraseña.')
+  })
+});
--- src/views/ApplicationForm/index.jsx
+++ src/views/ApplicationForm/index.jsx
@@ -2,52 +2,61 @@ import * as ApplicationForm from './components';
 import { validateSchemaAplicationForm } from './validation/validationFormAplication';
 import { Form, Formik } from 'formik';
 import { initialForm } from './data';
+import { useAuth } from '../../context/authContext';
+import { Navigate } from 'react-router-dom';
 
 const ApplicationFormPage = () => {
+  const { userData } = useAuth();
   return (
     <main className="container mx-auto my-12">
-      <ApplicationForm.OverviewCard />
-      <Formik
-        initialValues={initialForm}
-        validationSchema={validateSchemaAplicationForm}
-        onSubmit={async (values) => {
-          try {
-            console.log(values);
-          } catch (error) {
-            console.error(error);
-          }
-        }}
-      >
-        {({ errors, touched, isSubmitting }) => (
-          <Form>
-            <ApplicationForm.PersonalInformation
-              errors={errors}
-              touched={touched}
-            />
-            <ApplicationForm.EducationProfile
-              errors={errors}
-              touched={touched}
-            />
-            <ApplicationForm.JobProfile errors={errors} touched={touched} />
-            <ApplicationForm.ExperienceAndWork
-              errors={errors}
-              touched={touched}
-            />
-            <ApplicationForm.JobType errors={errors} touched={touched} />
-            <button
-              type="submit"
-              className="rounded-lg border hover:border text-lg p-2 w-2/3 font-regular bg-white hover:bg-transparent hover:md:bg-mid-blue md:bg-dark-purple hover: text-dark-purple mb:text-white hover:text-white md:text-white font-sans"
-            >
-              Enviar Formulario
-            </button>
-            {isSubmitting ? (
-              <div>
-                <p>Enviando datos...</p>
-              </div>
-            ) : null}
-          </Form>
-        )}
-      </Formik>
+      {!userData ? (
+        <Navigate to={'/login'} />
+      ) : (
+        <>
+          <ApplicationForm.OverviewCard />
+          <Formik
+            initialValues={initialForm}
+            validationSchema={validateSchemaAplicationForm}
+            onSubmit={async (values) => {
+              try {
+                console.log(values);
+              } catch (error) {
+                console.error(error);
+              }
+            }}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form>
+                <ApplicationForm.PersonalInformation
+                  errors={errors}
+                  touched={touched}
+                />
+                <ApplicationForm.EducationProfile
+                  errors={errors}
+                  touched={touched}
+                />
+                <ApplicationForm.JobProfile errors={errors} touched={touched} />
+                <ApplicationForm.ExperienceAndWork
+                  errors={errors}
+                  touched={touched}
+                />
+                <ApplicationForm.JobType errors={errors} touched={touched} />
+                <button
+                  type="submit"
+                  className="rounded-lg border hover:border text-lg p-2 w-2/3 font-regular bg-white hover:bg-transparent hover:md:bg-mid-blue md:bg-dark-purple hover: text-dark-purple mb:text-white hover:text-white md:text-white font-sans"
+                >
+                  Enviar Formulario
+                </button>
+                {isSubmitting ? (
+                  <div>
+                    <p>Enviando datos...</p>
+                  </div>
+                ) : null}
+              </Form>
+            )}
+          </Formik>
+        </>
+      )}
     </main>
   );
 };
