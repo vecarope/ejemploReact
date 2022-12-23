c9114d64f076b5dfd397ff623d290a7d5b286ed0
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-12-23 19:52:43 -0300

    feat: add personal information data to post form

--- src/hooks/postAxios.js
+++ src/hooks/postAxios.js
@@ -19,8 +19,52 @@ export const postLoginAxios = async (values) => {
 };
 
 export const postContactCompany = async (values) => {
-    try {
+  try {
     const { data } = await apiClient.post('/contact-company', values);
+    return data;
+  } catch ({ error }) {
+    return error.message;
+  }
+};
+
+export const postApplicationForm = async (values) => {
+  const dataUser = localStorage.getItem('user');
+  const userForm = JSON.parse(dataUser);
+
+  console.log('FORM APPP POST-->', values);
+  const appicationData = [
+    {
+      phoneNumber: values.phoneNumber,
+      city: values.city,
+      country: values.country,
+      gender: values.gender,
+      employmentStatusCurrent: values.employmentStatusCurrent,
+      stack: values.stack.toString(),
+      educationalLevel: parseInt(values.educationalLevel),
+      educationStatusCurrent: values.educationStatusCurrent,
+      englishLevel: values.englishLevel,
+      additionalToolsComment: values.additionalToolsComment.toString(),
+      cvUrl: values.cvUrl,
+      linkedinUrl: values.linkedinUrl,
+      githubUrl: values.githubUrl,
+      portfolioUrl: values.portfolioUrl,
+      featuredProject: values.featuredProject,
+      devExperience: parseInt(values.devExperience.split(',')[1].trim()),
+      idealWorkComment: values.idealWorkComment,
+      workAvailability: values.workAvailability.toString(),
+      relocationOption: parseInt(values.relocationOption.split(',')[1].trim()),
+      visa: values.visa.toString(),
+      userId: userForm.id
+    },
+    [{}],
+    []
+  ];
+  const token = localStorage.getItem('token');
+
+  console.log('APPLICATION POST-->', appicationData);
+
+  try {
+    const { data } = await apiClient.post(`/user-workprofile/${token}`, values);
     return data;
   } catch ({ error }) {
     return error.message;
