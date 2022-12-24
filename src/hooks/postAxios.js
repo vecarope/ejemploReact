d4dfb7354458b5dc637d57d72799f790bb8cf7ae
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-12-24 13:26:08 -0300

    feat: add soft skills objet for form

--- src/hooks/postAxios.js
+++ src/hooks/postAxios.js
@@ -34,30 +34,71 @@ export const postApplicationForm = async (values) => {
   console.log('FORM APPP POST-->', values);
   const appicationData = [
     {
-      phoneNumber: values.phoneNumber,
+      phone_number: values.phoneNumber,
       city: values.city,
       country: values.country,
       gender: values.gender,
       employmentStatusCurrent: values.employmentStatusCurrent,
       stack: values.stack.toString(),
-      educationalLevel: parseInt(values.educationalLevel),
-      educationStatusCurrent: values.educationStatusCurrent,
-      englishLevel: values.englishLevel,
-      additionalToolsComment: values.additionalToolsComment.toString(),
-      cvUrl: values.cvUrl,
-      linkedinUrl: values.linkedinUrl,
-      githubUrl: values.githubUrl,
-      portfolioUrl: values.portfolioUrl,
-      featuredProject: values.featuredProject,
-      devExperience: parseInt(values.devExperience.split(',')[1].trim()),
-      idealWorkComment: values.idealWorkComment,
-      workAvailability: values.workAvailability.toString(),
-      relocationOption: parseInt(values.relocationOption.split(',')[1].trim()),
+      educational_level: parseInt(values.educationalLevel),
+      education_status: values.educationStatusCurrent,
+      english_level: values.englishLevel,
+      comment: values.additionalToolsComment.toString(),
+      cv_url: values.cvUrl,
+      linkedin_url: values.linkedinUrl,
+      github_url: values.githubUrl,
+      portfolio_url: values.portfolioUrl,
+      featured_project: values.featuredProject,
+      dev_experience: parseInt(values.devExperience.split(',')[1].trim()),
+      ideal_work_comment: values.idealWorkComment,
+      work_availability: values.workAvailability.toString(),
+      relocation_option: parseInt(values.relocationOption.split(',')[1].trim()),
       visa: values.visa.toString(),
       userId: userForm.id
     },
-    [{}],
-    []
+    [
+      {
+        name: values.name,
+        institute_name: values.institute_name,
+        type: values.type,
+        workProfileId: 1
+      },
+      {
+        name: values.name2,
+        institute_name: values.institute_name2,
+        type: values.type2,
+        workProfileId: 1
+      }
+    ],
+    [
+      {
+        // Lenguajes,
+      }
+    ],
+    [
+      {
+        // Frameworks
+      }
+    ],
+    [
+      {
+        //Tools
+      }
+    ],
+    [
+      // Soft Skills
+      values.softSkills[1]
+        ? values.softSkills.map((skill) => ({
+            workProfileId: 1,
+            softSkillId: parseInt(skill.split(',')[1].trim()),
+            name: skill.split(',')[0].trim()
+          }))
+        : {
+            workProfileId: 1,
+            softSkillId: parseInt(values.softSkills[0].split(',')[1].trim()),
+            name: values.softSkills[0].split(',')[0].trim()
+          }
+    ]
   ];
   const token = localStorage.getItem('token');
 
