c19037d62599410257bef0b2afeb968b2bb4e424
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-12-29 21:26:07 -0300

    fix: routes user dashboard

--- src/components/User/DashboardData.jsx
+++ src/components/User/DashboardData.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import {AiOutlineHome} from 'react-icons/ai'
-import {FiUserCheck} from 'react-icons/fi'
-import {CgFileDocument} from 'react-icons/cg'
+import React from 'react';
+import { AiOutlineHome } from 'react-icons/ai';
+import { FiUserCheck } from 'react-icons/fi';
+import { CgFileDocument } from 'react-icons/cg';
 
-export const DashboardData=[
-        {
-            title:'Home', 
-            icon:<AiOutlineHome/>,
-            path:'/user'
-        },
-        {
-            title:'Perfil', 
-            icon:<FiUserCheck/>,
-            path:'/profile'
-        },
-        {
-            title:'Test técnicos', 
-            icon:<CgFileDocument/>,
-            path:'/test'
-        }
-    ]
-
+export const DashboardData = [
+  {
+    title: 'Home',
+    icon: <AiOutlineHome />,
+    path: '/user'
+  },
+  {
+    title: 'Perfil',
+    icon: <FiUserCheck />,
+    path: '/user/profile'
+  },
+  {
+    title: 'Test técnicos',
+    icon: <CgFileDocument />,
+    path: '/user/test'
+  }
+];
