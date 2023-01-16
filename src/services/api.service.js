a60fa5bebe34e0f94934952e1605417cdb8d78fc
TheFranciscoMolina <adrii.barzola3@gmail.com>
2023-01-16 17:05:18 -0300

    fix: url to env file

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3001/api'
-  /* Este es el acceso a la api desde la variable de entorno. Descomentarlo cuando este todo ok: */
-  // baseURL: process.env.API_URL
+  baseURL: `${process.env.REACT_APP_API_URL}`
 });
 
 apiClient.interceptors.request.use(function (config) {
