4cb887d04d2e882c053d1f733acf0eec62cb7c30
TheFranciscoMolina <adrii.barzola3@gmail.com>
2023-01-16 15:46:01 -0300

    feat: add variable of enviromento for api db

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.API_URL
+  baseURL: 'http://localhost:3001/api'
+  /* Este es el acceso a la api desde la variable de entorno. Descomentarlo cuando este todo ok: */
+  // baseURL: process.env.API_URL
 });
 
 apiClient.interceptors.request.use(function (config) {
   config.headers['Authorization'] = localStorage.getItem('token');
-
-  console.log(config.headers);
   return config;
 });
 
