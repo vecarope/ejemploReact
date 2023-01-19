fd1b96a1b9333a8b83ba9c7e6f41e6ca52bbb4e4
TheFranciscoMolina <adrii.barzola3@gmail.com>
2023-01-19 16:02:14 -0300

    fix: env function for service

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL:'http://localhost:3001/api'
+  baseURL: `${process.env.REACT_APP_API_URL}`
 });
 
 apiClient.interceptors.request.use(function (config) {
