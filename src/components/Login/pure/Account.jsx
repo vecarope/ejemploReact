469d29847121ac5d93141c498bddea22ee1fc62e
vecarope <vecarope@gmail.com>
2023-01-24 13:25:23 -0300

    fix buttons logins

--- src/components/Login/pure/Account.jsx
+++ src/components/Login/pure/Account.jsx
@@ -42,10 +42,10 @@ const Account = ({ change }) => {
   return (
     <>
     <div className="grid grid-cols-1 md:grid-cols-4 gap-1 p-4 max-w-xs md:max-w-lg mx-auto">
-    <button className="btn btn-login text-dark-text md:col-start-2 bg-white" onClick={() => change('email')}>
+    <button className="btn btn-login text-dark-text md:col-start-2 bg-white  hover:text-white" onClick={() => change('email')}>
     <AiOutlineMail  className="h-6 w-6 mr-1" />Ingresa con tu mail
     </button>
-    <button onClick={handleGoogleSignin} className="btn btn-login bg-white text-dark-text "><AiFillGoogleCircle  className="h-6 w-6 mr-1" />Google</button>
+    <button onClick={handleGoogleSignin} className="btn btn-login bg-white text-dark-text "><AiFillGoogleCircle  className="h-6 w-6 mr-1" />Ingresa con Google</button>
     <button onClick='' className="btn btn-login bg-dark-purple text-white "><BsLinkedin  className="h-6 w-6 mr-1" /> Ingresa con LinkedIn</button>
     <button onClick={handleFacebookSignin} className="btn btn-login bg-[#3b5998] text-white"><AiFillFacebook  className="h-6 w-6 mr-1" />Ingresa con Facebook</button>
     <button onClick={handleGithubSignin} className="btn btn-login bg-dark-text text-white "><BsGithub  className="h-6 w-6 mr-1" />Ingresa con Github</button>
