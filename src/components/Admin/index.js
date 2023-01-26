9890d541cc1b30b86febe01407d7ebfbc6c0ada5
Felipe Gatica <pgatilo20@gmail.com>
2023-01-26 13:34:36 -0300

    fix/fgatica/view-design-databse-devlangague-toolsTable

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,4 +1,3 @@
 export { default as DevlangagueTable } from './DevlangagueTable';
 export { default as DatabaseTable } from './DatabaseTable';
 export { default as ToolsTable } from './ToolsTable';
-export { default as CompanyTable } from './CompanyTable';
