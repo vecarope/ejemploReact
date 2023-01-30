905a735c957a8264293747de88afddf3905e4aae
TheFranciscoMolina <fdm.molina@gmail.com>
2023-01-30 20:04:17 -0300

    fix: add function by update year automatic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from '../../assets/images/DEV-IMAGOTIPO-WHITE-HORIZONTAL.png';
 import { AiOutlineLinkedin } from 'react-icons/ai';
-
+const year = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer className="footer items-center p-4 bg-[#1E239A] text-neutral-content text-center flex">
       <div className="items-center grid-flow-col">
-          <img className="w-40" src={logo} alt="Logo" />
+        <img className="w-40" src={logo} alt="Logo" />
       </div>
-      <p className="justify-center mx-auto">Copyright © 2022 - Devsafio</p>
+      <p className="justify-center mx-auto">Copyright © {year} - Devsafio</p>
       <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <a
           className="w-40"
