df820cee0c12d57907378c3df2f0f6cf9be875b7
TheFranciscoMolina <fdm.molina@gmail.com>
2023-02-01 13:54:02 -0300

    fix: views responsive in error component

--- src/views/Error404/index.jsx
+++ src/views/Error404/index.jsx
@@ -12,26 +12,21 @@ const Error404 = () => {
   }, [navigate]);
 
   return (
-    <section>
-      <div className="flex-col">
-        <div>
-          <img
-            className="mx-auto w-full sm:object-contain sm:w-1/2"
-            src={error404}
-            alt="error img"
-          />
-        </div>
-        <div className="text-center mx-3  sm:m-10">
-          <h1 className=" mt-8 mb-5 text-5xl sm:text-7xl font-bold">Oops</h1>
-          <br />
-          <p className="bg-[error404] mb-5  text-3xl sm:text-4xl ">
+    <section className="min-h-screen align-middle justify-center">
+      <div className="flex-col  align-middle mx-auto">
+        <img
+          className="mx-auto w-full sm:object-contain md:w-1/2 lg:w-1/3"
+          src={error404}
+          alt="error img"
+        />
+        <div className="text-center mx-3 sm:m-10">
+          <h1 className=" mt-8 mb-5 text-4xl md:text-5xl font-bold">Oops</h1>
+          <p className="bg-[error404] mb-5 text-base  md:text-lg lg:text-xl">
             <strong>Parece que perdiste la ruta</strong>, presiona el siguiente
-            botón
-            <br />
-            para encontrar el camino a tu primer empleo como Developer{' '}
+            botón para encontrar el camino a tu primer empleo como Developer.
           </p>
-          <button onClick={goLanding} className="btn btn-blue mb-8">
-            DEVSAFIO
+          <button onClick={goLanding} className="btn btn-blue ">
+            Volver al inicio
           </button>
         </div>
       </div>
